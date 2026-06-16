63b474dfbcf566193f17b3793d5f9af870301db2
CTK1215 <ctkelly1277@gmail.com>
2026-06-16 14:41:52 -0700

    Completed WeeklyChallengesSet06

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq; 
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +8,86 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null) return false;
+
+            StringComparison comparison = ignoreCase
+                ? StringComparison.OrdinalIgnoreCase
+                : StringComparison.Ordinal;
+
+            foreach (string w in words)
+            {
+                if (w != null && w.Equals(word, comparison)) return true;
+            }
+            return false;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num < 2) return false;
+            if (num == 2) return true;
+            if (num % 2 == 0) return false;
+
+            // Only need to check odd divisors up to sqrt(num)
+            int limit = (int)Math.Sqrt(num);
+            for (int i = 3; i <= limit; i += 2)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(str)) return -1;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                bool isUnique = true;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (i != j && str[j] == str[i])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique) return i;
+            }
+            return -1;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0) return 0;
+
+            int maxRun = 1;
+            int currentRun = 1;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentRun++;
+                    if (currentRun > maxRun) maxRun = currentRun;
+                }
+                else
+                {
+                    currentRun = 1;
+                }
+            }
+            return maxRun;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || n <= 0) return new double[0];
+
+            List<double> result = new List<double>();
+            for (int i = n - 1; i < elements.Count; i += n)
+            {
+                result.Add(elements[i]);
+            }
+            return result.ToArray();
         }
     }
 }
